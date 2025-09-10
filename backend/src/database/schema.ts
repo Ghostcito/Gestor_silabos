a6e16d8ee6d76db63714f0df8fa80bf67bdbd3e7
Ghostcito <jmguticasani1254@gmail.com>
2025-09-10 16:58:11 -0500

    Creando schema

---
+++ backend/src/database/schema.ts
@@ -0,0 +1,19 @@
+import {
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const silabos = pgTable("silabos", {
+  id: integer().primaryKey(),
+  codigo: varchar("codigo", { length: 10 }).notNull().unique(), // Ej: "CS101"
+  nombre: varchar("nombre", { length: 100 }).notNull(), // Ej: "Programación I"
+  creditos: integer("creditos").notNull(),
+  ciclo: integer("ciclo").notNull(), // Semestre/ciclo académico
+  descripcion: text("descripcion"),
+  profesor: varchar("profesor", { length: 100 }).notNull(),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+  updated_at: timestamp("updated_at").notNull().defaultNow(),
+});
